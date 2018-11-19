ed11d36fea1f07ac2f6ee3f63ab40afcdbfbb6f1
Scott Straley <scott@maidbot.com>
2018-11-19 11:30:38 -0600

    adding latest build

--- docs/src/game-tile.js
+++ docs/src/game-tile.js
@@ -1,10 +1,10 @@
 define(["../node_modules/@polymer/polymer/polymer-element.js", "../node_modules/@polymer/iron-icon/iron-icon.js"], function (_polymerElement, _ironIcon) {
   "use strict";
 
-  function _templateObject_d7745e40ec1b11e88b167d747c0bbd28() {
+  function _templateObject_cb191f50ec2011e8963c45481ebbea09() {
     var data = babelHelpers.taggedTemplateLiteral(["\n      <style>\n        :host {\n          display: block;\n          position: absolute;\n          width: var(--game-tile-size);\n          height: var(--game-tile-size);\n          --iron-icon-height: var(--game-tile-size);\n          --iron-icon-width: var(--game-tile-size);\n          cursor: pointer;\n        }\n        :host([state=\"flipped\"]) {\n          display: none;\n        }\n      </style>\n      <iron-icon\n        icon=\"game:[[icon]]\"\n        role=\"img\"\n        class=\"tile\"\n        style=\"top: calc([[x]] * var(--game-tile-size)); left: calc([[y]] * var(--game-tile-size));\"\n      ></iron-icon>\n    "]);
 
-    _templateObject_d7745e40ec1b11e88b167d747c0bbd28 = function _templateObject_d7745e40ec1b11e88b167d747c0bbd28() {
+    _templateObject_cb191f50ec2011e8963c45481ebbea09 = function _templateObject_cb191f50ec2011e8963c45481ebbea09() {
       return data;
     };
 
@@ -35,7 +35,7 @@ define(["../node_modules/@polymer/polymer/polymer-element.js", "../node_modules/
     }, {
       key: "template",
       get: function get() {
-        return (0, _polymerElement.html)(_templateObject_d7745e40ec1b11e88b167d747c0bbd28());
+        return (0, _polymerElement.html)(_templateObject_cb191f50ec2011e8963c45481ebbea09());
       }
     }]);
 
